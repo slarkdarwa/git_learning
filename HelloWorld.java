a7041dc58136569463f19ab78bfdcd5b745af803
slarkdarwa <slarkdar@outlook.com>
2020-06-20 00:56:18 +0800

    commit  modified file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
        public static void main(String[] args) {
-              System.out.println("我的第一个Java程序：Hello World。");
+              System.out.println("我的第一个Java程序：Hello World。test Git");
        }
 }

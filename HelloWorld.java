68de3f7058476312947acb98427e8ce13015cfca
slarkdarwa <slarkdar@outlook.com>
2020-06-20 09:00:41 +0800

    Oh, commit addtion

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
        public static void main(String[] args) {
-              System.out.println("我的第一个Java程序：Hello World。Test Git");
+              System.out.println("My first java program：Hello World。Test Git");
        }
 }

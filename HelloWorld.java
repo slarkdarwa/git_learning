fa848118eec01e2cbbf3df57b010d4d1d60ae3ce
BonjeanLee <bjwang2014@hotmail.com>
2020-06-26 09:03:10 +0800

    test git push remote repository

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
        public static void main(String[] args) {
-              System.out.println("My first java program：Hello World。Test Git");
+              System.out.println("My first java program:Hello World! Test git.");
        }
 }

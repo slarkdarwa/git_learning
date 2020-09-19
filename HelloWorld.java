7a50b2d201f2833da392189a63323512f51628de
slarkdarwa <slarkdar@outlook.com>
2020-09-19 22:29:17 +0800

    test ssh pk

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
        public static void main(String[] args) {
               System.out.println("Test git. local");
               System.out.println("Test git. remote");
-              System.out.println("Test git. ssh key");
+              System.out.println("Test git. ssh key ok");
        }
 }
